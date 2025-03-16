942515d0eee5825a2f8d5ef993ffae2a93506fca
VT Giang <vtgiang2k4@gmail.com>
2025-03-16 23:25:59 +0700

    layout client

---
+++ resources/views/client/layout/master.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>@yield('title')</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Mukta:300,400,700"> 
+    <link rel="stylesheet" href="{{ asset('client/fonts/icomoon/style.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('client/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('client/css/magnific-popup.css') }}">
+    <link rel="stylesheet" href="{{ asset('client/css/jquery-ui.css') }}">
+    <link rel="stylesheet" href="{{ asset('client/css/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('client/css/owl.theme.default.min.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('client/css/aos.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('client/css/style.css') }}">
+
+    
+  </head>
+  <body>
+  
+  <div class="site-wrap">
+    <header class="site-navbar" role="banner">
+        @include('client.layout.navbar')
+    </header>
+
+    @include('client.layout.header')
+
+    <div class="site-section site-blocks-2">
+        @include('client.layout.categories')
+    </div>
+
+    <div class="site-section block-3 site-blocks-2 bg-light">
+        @yield('content')
+    </div>
+
+    <footer class="site-footer border-top">
+        @include('client.layout.footer')
+    </footer>
+  </div>
+
+  <script src="{{ asset('client/js/jquery-3.3.1.min.js') }}"></script>
+  <script src="{{ asset('client/js/jquery-ui.js') }}"></script>
+  <script src="{{ asset('client/js/popper.min.js') }}"></script>
+  <script src="{{ asset('client/js/bootstrap.min.js') }}"></script>
+  <script src="{{ asset('client/js/owl.carousel.min.js') }}"></script>
+  <script src="{{ asset('client/js/jquery.magnific-popup.min.js') }}"></script>
+  <script src="{{ asset('client/js/aos.js') }}"></script>
+
+  <script src="{{ asset('client/js/main.js') }}"></script>
+
+    
+  </body>
+</html>
